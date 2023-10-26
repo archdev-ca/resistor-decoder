6a42e150afa5f6cfb0c2f9764ae49d5a33230ddc
acgelonga <acgelonga@gmail.com>
2023-10-26 18:12:00 -0500

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import styled from "styled-components";
+
+const ResistorWrapper = styled("div")`
+  width: 500px;
+  height: 150px;
+  background-color: #f9ddbf;
+  border-radius: 75px;
+`;
+
+function App() {
+  return (
+    <>
+      <main>
+        <ResistorWrapper />
+      </main>
+    </>
+  );
+}
+
+export default App;
